9607144ca6bb4ddceb97d9fb5adfdc43a9f7fc8e
ttwards <xuwenxi0517@gmail.com>
2024-12-01 20:44:50 +0800

    updated imu

--- yunqiu/src/main.c
+++ yunqiu/src/main.c
@@ -44,37 +44,16 @@ const struct device *cpu_dev = DEVICE_DT_GET(CPU_NODE);
 const struct device *can_dev = DEVICE_DT_GET(CANBUS_NODE);
 const struct device *motor1  = DEVICE_DT_GET(MOTOR1_NODE);
 const struct device *motor2  = DEVICE_DT_GET(MOTOR2_NODE);
-const struct device *motor3  = DEVICE_DT_GET(MOTOR3_NODE);
-const struct device *sbus    = DEVICE_DT_GET(SBUS_NODE);
-const struct device *accel   = DEVICE_DT_GET(IMU_ACCEL_NODE);
-const struct device *gyro    = DEVICE_DT_GET(IMU_GYRO_NODE);
+// const struct device *motor3  = DEVICE_DT_GET(MOTOR3_NODE);
+const struct device *sbus  = DEVICE_DT_GET(SBUS_NODE);
+const struct device *accel = DEVICE_DT_GET(IMU_ACCEL_NODE);
+const struct device *gyro  = DEVICE_DT_GET(IMU_GYRO_NODE);
 
 k_tid_t feedback_tid = 0;
 
 float motor1_rpm = 0;
 float motor2_rpm = 0;
-float motor3_rpm = 0;
-
-// 定义重力加速度常数
-const float GRAVITY_EARTH = 9.80665f;
-// 加速度计全量程（g）
-const float accel_full_scale = 16.0f;
-const float gyro_full_scale  = 2000.0f;
-// 计算缩放因子
-float accel_scale = (GRAVITY_EARTH * accel_full_scale) / 32768.0f;
-float gyro_scale  = (gyro_full_scale) / 32768.0f;
-#define BMI08X_TEMP_OFFSET 23
-
-// 温度转换函数
-static float convert_temp_bmi08x(const struct sensor_value *temp_data) {
-    // 按照驱动中的计算方式：
-    // temp_micro = BMI08X_TEMP_OFFSET * 1000000ULL + temp_raw * 31250ULL
-    int32_t temp_micro = (int32_t)temp_data->val1 * 1000000 + temp_data->val2;
-
-    // 转换为实际温度值
-    float temp = (float)(temp_micro - BMI08X_TEMP_OFFSET * 1000000ULL) / 31250.0f;
-    return temp + BMI08X_TEMP_OFFSET;
-}
+// float motor3_rpm = 0;
 
 /* CAN Feedback to console*/
 K_THREAD_STACK_DEFINE(feedback_stack_area, 4096); // 定义线程栈
@@ -82,7 +61,7 @@ void console_feedback(void *arg1, void *arg2, void *arg3) {
     while (1) {
         LOG_INF("rpm: motor1: %.2f %.2f\n", (double)motor1_rpm, (double)motor_get_speed(motor1));
         LOG_INF("rpm: motor2: %.2f %.2f\n", (double)motor2_rpm, (double)motor_get_speed(motor2));
-        LOG_INF("rpm: motor3: %.2f %.2f\n", (double)motor3_rpm, (double)motor_get_speed(motor3));
+        // LOG_INF("rpm: motor3: %.2f %.2f\n", (double)motor3_rpm, (double)motor_get_speed(motor3));
         struct sensor_value accel_data[3];
         struct sensor_value gyro_data[3];
         struct sensor_value temp_data;
@@ -106,7 +85,6 @@ void console_feedback(void *arg1, void *arg2, void *arg3) {
         // LOG_INF("TEMP MSB: %d LSB: %d\n", temp_data.val1, temp_data.val2);
         LOG_INF("Accel %.2f m/s^2\n", (double)accel_c);
         LOG_INF("Gyro %.2f\n", (double)gryo_c);
-
         k_msleep(500);
     }
 }
@@ -120,26 +98,29 @@ void console_feedback(void *arg1, void *arg2, void *arg3) {
 /*
 v_3 = (1/sqrt(2)) * (v_x - v_Y)
 */
-static void chassis_calc(float vert_x, float vert_y) {
-    float vert_x_rpm = vert_x * 120;
-    float vert_y_rpm = vert_y * 120;
-    motor1_rpm       = vert_x_rpm;
-    motor3_rpm       = vert_y_rpm;
-    motor2_rpm       = (-vert_x_rpm - vert_y_rpm) * 0.707f;
-    motor_set_speed(motor1, motor1_rpm);
-    motor_set_speed(motor2, motor2_rpm);
-    motor_set_speed(motor3, motor3_rpm);
+// static void chassis_calc(float vert_x, float vert_y) {
+//     float vert_x_rpm = vert_x * 120;
+//     float vert_y_rpm = vert_y * 120;
+//     motor1_rpm       = vert_x_rpm;
+//     motor3_rpm       = vert_y_rpm;
+//     motor2_rpm       = (-vert_x_rpm - vert_y_rpm) * 0.707f;
+//     motor_set_speed(motor1, motor1_rpm);
+//     motor_set_speed(motor2, motor2_rpm);
+//     motor_set_speed(motor3, motor3_rpm);
+// }
+
+int sbus_get_bool(const struct device *sbus, int channel) {
+    sbus_get_digit(sbus, channel);
+    return (int)sbus_get_percent(sbus, channel);
 }
 
 int main(void) {
 
     motor_set_zero(motor1);
     motor_set_zero(motor2);
-    motor_set_zero(motor3);
 
     motor_set_angle(motor1, 0);
     motor_set_angle(motor2, 0);
-    motor_set_angle(motor3, 0);
 
     /* Start Feedback thread*/
     struct k_thread feedback_thread_data;
@@ -148,7 +129,11 @@ int main(void) {
                                    K_THREAD_STACK_SIZEOF(feedback_stack_area), console_feedback,
                                    (void *)motor1, NULL, NULL, 0, 0, K_MSEC(300));
     while (1) {
-        chassis_calc(sbus_get_percent(sbus, 1), sbus_get_percent(sbus, 3));
+        // chassis_calc(sbus_get_percent(sbus, 1), sbus_get_percent(sbus, 3));
+        motor1_rpm = 450 * sbus_get_percent(sbus, 3);
+        motor2_rpm = 450 * sbus_get_percent(sbus, 3);
+        motor_set_speed(motor1, motor1_rpm);
+        motor_set_speed(motor2, motor2_rpm);
         k_msleep(20);
     }
 }
