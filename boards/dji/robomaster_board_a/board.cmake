49a93f0f80d95cecfa6bfbdc3877db1f3b7425e6
NiZenMeZhiDao <12313401@mail.sustech.edu.cn>
2025-01-19 01:39:09 +0800

    Added support for robomaster_board_a

---
+++ boards/dji/robomaster_board_a/board.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(openocd)
+board_runner_args(pyocd "--target=STM32F427II")
+board_runner_args(jlink "--device=STM32F427II" "--speed=4000")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
