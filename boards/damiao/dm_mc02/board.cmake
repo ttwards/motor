ed6e10bae0cb26d49ed673961b89f0fcf34a33ae
Wenxi Xu <xuwenxi0517@gmail.com>
2025-02-27 23:01:35 +0800

    Updated support for dm_mc02

--- boards/damiao/dm_mc02/board.cmake
+++ boards/damiao/dm_mc02/board.cmake
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # keep first
-board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd)
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(pyocd "--target=STM32H723VG")
 board_runner_args(jlink "--device=STM32H723VG" "--speed=4000")
 
 # keep first
-include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
