4bce20368520ebcc58e4eb79b2dc752dd9ffa430
Wenxi Xu <xuwenxi0517@gmail.com>
2025-04-04 09:39:24 +0800

    写完了串口通信

---
+++ include/ares/connectivity/uart_trans.c
@@ -0,0 +1,611 @@
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/time_units.h>
+#include <zephyr/toolchain.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/kernel.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zephyr/sys/crc.h>
+#include <zephyr/usb/usb_device.h>
+#include <ares/connectivity/uart_trans.h>
+#include <sys/types.h>
+#include "uart_async_adapter.h"
+#include "zephyr/sys/ring_buffer.h"
+
+LOG_MODULE_REGISTER(uart_trans, CONFIG_LOG_DEFAULT_LEVEL);
+
+// usb async adapter use millisecond as timeout
+// but uart use microsecond as timeout
+#if (CONFIG_USB_UART_ASYNC_ADAPTER)
+#define RX_INACTIVE_TIMEOUT 15
+#else
+#define RX_INACTIVE_TIMEOUT 15
+#endif
+
+#define CDC_NODE DT_CHOSEN(ares_usb)
+
+struct device *uart_dev = (struct device *)DEVICE_DT_GET(CDC_NODE);
+
+// serial buffer pool
+#define FUNC_REPL_SIZE   12
+#define BUF_SIZE         270
+#define SYNC_RX_BUF_SIZE (BUF_SIZE)
+#define SYNC_TX_BUF_SIZE (BUF_SIZE)
+
+#define MAX_PACK_COUNT 8
+#define MAX_FUNC_COUNT MAX_PACK_COUNT
+
+RING_BUF_DECLARE(uart_rx_rb, 350);
+
+K_MEM_SLAB_DEFINE(uart_rx_slab, SYNC_RX_BUF_SIZE, 8, 4);
+K_MEM_SLAB_DEFINE(func_ret_slab, (FUNC_REPL_SIZE + 4), 8, 4);
+
+uint8_t func_tx_bckup_cnt = 0;
+K_FIFO_DEFINE(func_tx_bckup_fifo);
+
+#define HEART_BEAT_DELAY 2
+
+uint8_t error_frame[8];
+
+static void uart_trans_heart_beat(struct k_timer *timer);
+K_TIMER_DEFINE(heart_beat_timer, uart_trans_heart_beat, NULL);
+
+bool online = false;
+uint32_t last_heart_beat = 0;
+uint32_t last_receive = 0;
+bool rx_throttle = false;
+static bool uart_initialized = false;
+
+static int8_t func_cnt = 0;
+static func_mapping_t func_table[MAX_PACK_COUNT];
+
+static int8_t sync_pack_cnt = 0;
+static uart_sync_pack_t sync_pack[MAX_PACK_COUNT];
+
+static void error_handle(uint16_t req_id, uint16_t error)
+{
+	GET_8BITS(error_frame, ERROR_ID_IDX) = req_id;
+	GET_16BITS(error_frame, ERROR_CODE_IDX) = error;
+	if (error != HEART_BEAT) {
+		LOG_ERR("Error code: %x, Request ID: %x", error, req_id);
+	} else {
+		last_heart_beat = k_cycle_get_32();
+	}
+	uart_tx(uart_dev, error_frame, 8, 10);
+}
+
+static uint16_t get_byte_length(uint8_t *data, size_t len)
+{
+	uint16_t byte_len = 0;
+	for (int i = len - 1; i >= 0; i--) {
+		if (data[i] == 0x00) {
+			byte_len++;
+		} else {
+			break;
+		}
+	}
+	return byte_len;
+}
+
+static bool proceed_tx_bck(uint8_t ID)
+{
+	// 创建一个临时队列
+	struct k_fifo temp_fifo;
+	k_fifo_init(&temp_fifo);
+	bool found = false;
+
+	// 临时存储所有数据项
+	uint8_t *item;
+	while ((item = k_fifo_get(&func_tx_bckup_fifo, K_NO_WAIT)) != NULL) {
+		if (GET_8BITS(item, REPL_REQ_ID_IDX + 4) == ID && !found) {
+			// 找到匹配的项目
+			uart_tx(uart_dev, (const uint8_t *)(item + 4), FUNC_REPL_SIZE, 20);
+			found = true;
+			continue;
+		} else {
+			// 保存到临时队列
+			k_fifo_put(&temp_fifo, item);
+		}
+	}
+
+	// 将所有项目放回原始队列
+	while ((item = k_fifo_get(&temp_fifo, K_NO_WAIT)) != NULL) {
+		k_fifo_put(&func_tx_bckup_fifo, item);
+	}
+
+	return found;
+}
+
+static int find_head_idx(uint8_t data[], size_t len)
+{
+	if (len < 8) {
+		return -1;
+	}
+
+	for (int i = 0; i < len - 4; i++) {
+		uint16_t head = GET_16BITS(data, i);
+		if (head == SYNC_FRAME_HEAD || head == FUNC_FRAME_HEAD || head == REPL_FRAME_HEAD ||
+		    head == REPL_FRAME_HEAD) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+static int find_tail_idx(uint8_t data[], size_t len)
+{
+	if (len < 8) {
+		return -1;
+	}
+
+	for (int i = 0; i < len - 4; i++) {
+		uint16_t tail = GET_16BITS(data, i);
+		if (tail == SYNC_FRAME_TAIL || tail == FUNC_FRAME_TAIL || tail == REPL_FRAME_TAIL) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+static int find_16bit(uint8_t data[], size_t len, uint16_t target)
+{
+	if (len < 8) {
+		return -1;
+	}
+
+	for (int i = 0; i < len - 4; i++) {
+		if (GET_16BITS(data, i) == target) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+static uart_sync_pack_t *find_pack(uint16_t ID)
+{
+	for (int i = 0; i < sync_pack_cnt; i++) {
+		if (ID == sync_pack[i].ID) {
+			return &sync_pack[i];
+		}
+	}
+	return NULL;
+}
+
+static func_mapping_t *find_func(uint16_t ID)
+{
+	for (int i = 0; i < func_cnt; i++) {
+		if (ID == func_table[i].id) {
+			return &func_table[i];
+		}
+	}
+	return NULL;
+}
+
+static void uart_trans_heart_beat(struct k_timer *timer)
+{
+	if (online) {
+		if (k_cyc_to_ms_near32(k_cycle_get_32() - last_heart_beat) <= HEART_BEAT_DELAY &&
+		    k_cyc_to_ms_near32(k_cycle_get_32() - last_receive) >= HEART_BEAT_DELAY) {
+			LOG_ERR("Connection lost for %d ms.",
+				k_cyc_to_ms_near32(k_cycle_get_32() - last_receive));
+			online = false;
+			last_heart_beat = k_cycle_get_32();
+		}
+	}
+	error_handle(HEARTBEAT_ID, HEART_BEAT);
+	last_heart_beat = k_cycle_get_32();
+}
+
+static void parse_sync(uint8_t data[], int8_t len)
+{
+	LOG_HEXDUMP_DBG(data, len, "SYNC frame received");
+	uint8_t crc8 = 0x00;
+	int data_len;
+	data_len = len - 8;
+	uart_sync_pack_t *pack = find_pack(GET_16BITS(data, 2));
+	if (pack == NULL) {
+		LOG_ERR("Cannot find corresponding ID in sync packs.");
+		error_handle(GET_16BITS(data, 2), UNKNOWN_SYNC);
+		return;
+	}
+	crc8 = crc8_ccitt(0x00, data + 2, data_len + 2);
+	if (crc8 != GET_8BITS(data, data_len + SYNC_CRC_OFFSET)) {
+		LOG_ERR("CRC mismatch.");
+
+		error_handle(GET_16BITS(data, 2), UNMATCH_CRCx);
+
+		return;
+	}
+	if (pack->cb != NULL) {
+		pack->cb(SYNC_PACK_STATUS_WRITE);
+	}
+	memcpy(pack->data, data + SYNC_DATA_IDX, data_len);
+	return;
+}
+
+static void parse_func(uint8_t data[], int8_t len)
+{
+	uint8_t crc8 = 0x00;
+	func_mapping_t *map = find_func(GET_16BITS(data, FUNC_ID_IDX));
+	if (map == NULL) {
+		LOG_ERR("Cannot find corresponding ID in func mappings.");
+		error_handle(0, UNKNOWN_FUNC);
+	}
+	crc8 = crc8_ccitt(0x00, data + 2, 15);
+	if (crc8 != GET_8BITS(data, FUNC_CRC_IDX)) {
+		error_handle(GET_16BITS(data, FUNC_REQ_IDX), UNMATCH_CRCx);
+		return;
+	}
+	void *ret = map->cb((void *)GET_32BITS(data, FUNC_ARG1_IDX),
+			    (void *)GET_32BITS(data, FUNC_ARG2_IDX),
+			    (void *)GET_32BITS(data, FUNC_ARG3_IDX));
+
+	if (func_tx_bckup_cnt == MAX_FUNC_COUNT) {
+		uint8_t *rm_frame = k_fifo_get(&func_tx_bckup_fifo, K_NO_WAIT);
+		if (rm_frame != NULL) {
+			func_tx_bckup_cnt--;
+			k_mem_slab_free(&func_ret_slab, &rm_frame);
+		} else {
+			LOG_ERR("Failed to free memory for FUNC frame.");
+			error_handle(GET_16BITS(data, FUNC_REQ_IDX), CLEANED_PACK);
+			return;
+		}
+	}
+
+	uint8_t *repl_frame;
+	int err = k_mem_slab_alloc(&func_ret_slab, (void **)&repl_frame, K_NO_WAIT);
+	repl_frame += 4;
+	if (err != 0) {
+		LOG_ERR("Failed to allocate memory for FUNC frame.");
+		error_handle(GET_16BITS(data, FUNC_ID_IDX), CLEANED_PACK);
+		return;
+	}
+	GET_16BITS(repl_frame, REPL_HEAD_IDX) = REPL_FRAME_HEAD;
+	GET_16BITS(repl_frame, REPL_FUNC_ID_IDX) = GET_16BITS(data, FUNC_ID_IDX);
+	GET_32BITS(repl_frame, REPL_RET_IDX) = (uint32_t)ret;
+	GET_8BITS(repl_frame, REPL_REQ_ID_IDX) = GET_16BITS(data, FUNC_REQ_IDX);
+	GET_16BITS(repl_frame, REPL_TAIL_IDX) = REPL_FRAME_TAIL;
+	crc8 = crc8_ccitt(0x00, (uint8_t *)repl_frame + 2, 7);
+	GET_8BITS(repl_frame, REPL_CRC_IDX) = crc8;
+
+	k_fifo_put(&func_tx_bckup_fifo, repl_frame - 4);
+	if (err != 0) {
+		LOG_ERR("Failed to put FUNC frame into FIFO.");
+		k_mem_slab_free(&func_ret_slab, (void **)(&repl_frame - 4));
+		return;
+	}
+	func_tx_bckup_cnt++;
+	uart_tx(uart_dev, (const uint8_t *)repl_frame, FUNC_REPL_SIZE, 20);
+	return;
+}
+
+static void parse_error(uint8_t data[], int8_t len)
+{
+	if (GET_16BITS(data, 6) != ERROR_FRAME_TAIL) {
+		LOG_HEXDUMP_ERR(data, len, "Invalid ERROR frame.");
+		LOG_ERR("Invalid ERROR frame tail. %x", GET_16BITS(data, 7));
+		return;
+	}
+	uint8_t req_id = GET_8BITS(data, ERROR_ID_IDX);
+	if (req_id == HEARTBEAT_ID) {
+		return;
+	}
+	if (!proceed_tx_bck(req_id)) {
+		uart_sync_pack_t *pack = find_pack(sys_be16_to_cpu(GET_16BITS(data, 2)));
+		if (pack == NULL) {
+			LOG_ERR("Cannot find corresponding ID from error "
+				"frame.");
+			error_handle(req_id, CLEANED_PACK);
+			return;
+		}
+		if (pack->cb != NULL) {
+			pack->cb(SYNC_PACK_STATUS_READ);
+		}
+		uart_trans_sync_flush(pack);
+	}
+	LOG_INF("Error frame received: id =%d; code=%d", req_id, GET_16BITS(data, ERROR_CODE_IDX));
+	return;
+}
+
+// 数据处理函数
+static void process_received_data()
+{
+	uint16_t head;
+	while (!ring_buf_is_empty(&uart_rx_rb)) {
+		if (ring_buf_get(&uart_rx_rb, (uint8_t *)&head, 2) != 2) {
+			ring_buf_put(&uart_rx_rb, (uint8_t *)&head, 1);
+			return;
+		}
+		if (head == SYNC_FRAME_HEAD) {
+			if (ring_buf_size_get(&uart_rx_rb) < 2) {
+				ring_buf_put(&uart_rx_rb, (uint8_t *)&head, 2);
+				return;
+			}
+			uint16_t id = 0;
+			if (ring_buf_get(&uart_rx_rb, (uint8_t *)&id, 2) != 2) {
+				return;
+			}
+			int16_t pack_size = -1;
+			for (int i = 0; i < sync_pack_cnt; i++) {
+				if (sync_pack[i].ID == id) {
+					pack_size = sync_pack[i].len + 8;
+					break;
+				}
+			}
+			if (pack_size == -1) {
+				LOG_ERR("Cannot find corresponding ID in sync packs.");
+				error_handle(0, UNKNOWN_SYNC);
+				return;
+			}
+			uint8_t buf[pack_size];
+			GET_16BITS(buf, 0) = SYNC_FRAME_HEAD;
+			GET_16BITS(buf, 2) = id;
+
+			int ret = ring_buf_get(&uart_rx_rb, buf + 4, pack_size - 4);
+			if (ret != pack_size - 4) {
+				ring_buf_put(&uart_rx_rb, buf, ret + 4);
+				return;
+			}
+			if (GET_16BITS(buf, pack_size - 2) != SYNC_FRAME_TAIL) {
+				LOG_ERR("Invalid SYNC frame tail.");
+				error_handle(id, UNKNOWN_TAIL);
+				return;
+			}
+
+			parse_sync(buf, pack_size);
+			continue;
+		} else if (head == FUNC_FRAME_HEAD) {
+			uint8_t buf[20];
+			GET_16BITS(buf, 0) = FUNC_FRAME_HEAD;
+			if (ring_buf_get(&uart_rx_rb, buf + 2, 18) != 18) {
+				LOG_ERR("Failed to get data from ring buffer.");
+				return;
+			}
+			parse_func(buf, 20);
+			continue;
+		} else if (head == ERROR_FRAME_HEAD) {
+			uint8_t buf[8];
+			GET_16BITS(buf, 0) = ERROR_FRAME_HEAD;
+			if (ring_buf_get(&uart_rx_rb, buf + 2, 6) != 6) {
+				LOG_ERR("Failed to get data from ring buffer.");
+				return;
+			}
+			parse_error(buf, 8);
+		} else {
+			// LOG_ERR("Invalid frame head.");
+			continue;
+		}
+	}
+}
+
+void uart_trans_sync_flush(uart_sync_pack_t *pack)
+{
+	if (pack == NULL) {
+		LOG_ERR("Sync pack is NULL.");
+		return;
+	}
+	memcpy(pack->buf + SYNC_DATA_IDX, pack->data, pack->len);
+	GET_16BITS(pack->buf, SYNC_HEAD_IDX) = SYNC_FRAME_HEAD;
+	GET_16BITS(pack->buf, SYNC_ID_IDX) = pack->ID;
+	pack->crc = crc8_ccitt(0x00, pack->buf + SYNC_ID_IDX, pack->len + 2);
+	GET_8BITS(pack->buf, pack->len + SYNC_CRC_OFFSET) = pack->crc;
+	GET_8BITS(pack->buf, pack->len + SYNC_CRC_OFFSET + 1) = 0x00;
+	GET_16BITS(pack->buf, pack->len + SYNC_TAIL_OFFSET) = SYNC_FRAME_TAIL;
+
+	LOG_HEXDUMP_DBG(pack->buf, pack->len + 8, "Sync data to send:");
+
+	uart_tx(uart_dev, pack->buf, pack->len + 8, 5);
+}
+
+uart_sync_pack_t *uart_trans_sync_add(uart_trans_data_t *data, uint16_t ID, size_t len,
+				      uart_trans_cb_t cb)
+{
+	if (sync_pack_cnt >= MAX_PACK_COUNT) {
+		LOG_ERR("Sync pack count exceeds maximum.");
+		return NULL;
+	}
+	sync_pack[sync_pack_cnt].data = data;
+	sync_pack[sync_pack_cnt].ID = sys_cpu_to_be16(ID);
+	sync_pack[sync_pack_cnt].len = len;
+	sync_pack[sync_pack_cnt].cb = cb;
+	sync_pack[sync_pack_cnt].buf = malloc(len + 8);
+	if (sync_pack[sync_pack_cnt].buf == NULL) {
+		LOG_ERR("Failed to allocate memory for SYNC frame.");
+		return NULL;
+	}
+	sync_pack_cnt++;
+	if (cb) {
+		cb(SYNC_PACK_STATUS_READ);
+	}
+	uart_trans_sync_flush(&sync_pack[sync_pack_cnt - 1]);
+
+	return &sync_pack[sync_pack_cnt - 1];
+}
+
+void uart_trans_func_add(uint16_t header, uart_trans_func_t cb)
+{
+	if (cb == NULL) {
+		LOG_ERR("Callback function is NULL.");
+		return;
+	}
+	if (func_cnt >= MAX_PACK_COUNT) {
+		LOG_ERR("Func count exceeds maximum.");
+		return;
+	}
+	func_table[func_cnt].id = sys_cpu_to_be16(header);
+	func_table[func_cnt].cb = cb;
+	func_cnt++;
+}
+
+void uart_trans_func_remove(uint16_t header)
+{
+	for (int i = 0; i < func_cnt; i++) {
+		if (func_table[i].id == header) {
+			func_cnt--;
+			func_table[i] = func_table[func_cnt];
+			return;
+		}
+	}
+	LOG_ERR("Cannot find corresponding ID from func table.");
+	return;
+}
+
+// async serial callback
+static void uart_callback(const struct device *dev, struct uart_event *evt, void *user_data)
+{
+	struct device *uart = user_data;
+	int err;
+
+	switch (evt->type) {
+	case UART_TX_DONE:
+		// LOG_INF("Tx sent %d bytes\n", evt->data.tx.len);
+		break;
+
+	case UART_TX_ABORTED:
+		LOG_INF("Tx aborted. Is the serial fast enough?\n");
+		break;
+
+	case UART_RX_RDY: {
+		LOG_DBG("Received data %d bytes\n", evt->data.rx.len);
+
+		if (evt->data.rx.len != 0) {
+			last_receive = k_cycle_get_32();
+		}
+
+		ring_buf_put(&uart_rx_rb, evt->data.rx.buf + evt->data.rx.offset, evt->data.rx.len);
+
+		break;
+	}
+
+	case UART_RX_BUF_REQUEST: {
+		uint8_t *buf;
+
+		err = k_mem_slab_alloc(&uart_rx_slab, (void **)&buf, K_NO_WAIT);
+		if (err != 0) {
+			LOG_ERR("Failed to allocate memory for RX buffer.");
+			uart_rx_disable(uart_dev);
+			error_handle(0, TOOHIGH_FREQ);
+			rx_throttle = true;
+			return;
+		}
+
+		err = uart_rx_buf_rsp(uart, buf, SYNC_RX_BUF_SIZE);
+		if (err != 0) {
+			LOG_ERR("Failed to provide new buffer.");
+			k_mem_slab_free(&uart_rx_slab, (void **)&buf);
+			uart_rx_disable(uart_dev);
+			error_handle(0, TOOHIGH_FREQ);
+			rx_throttle = true;
+			return;
+		}
+		break;
+	}
+
+	case UART_RX_BUF_RELEASED:
+		k_mem_slab_free(&uart_rx_slab, (void *)evt->data.rx_buf.buf);
+		if (rx_throttle) {
+			uint8_t *buf;
+			err = k_mem_slab_alloc(&uart_rx_slab, (void **)&buf, K_NO_WAIT);
+			if (err != 0) {
+				LOG_ERR("Failed to allocate memory for RX buffer.");
+				uart_rx_disable(uart_dev);
+				error_handle(0, TOOHIGH_FREQ);
+				return;
+			}
+			rx_throttle = false;
+			err = uart_rx_enable(uart_dev, buf, SYNC_RX_BUF_SIZE, RX_INACTIVE_TIMEOUT);
+			if (err != 0) {
+				LOG_ERR("Failed to enable RX.");
+				k_mem_slab_free(&uart_rx_slab, (void **)&buf);
+				uart_rx_disable(uart_dev);
+				error_handle(0, TOOHIGH_FREQ);
+				return;
+			}
+			LOG_INF("RX buffer released, re-enabled RX.");
+		}
+		break;
+
+	case UART_RX_DISABLED:
+		break;
+
+	case UART_RX_STOPPED:
+		break;
+	}
+}
+
+#if CONFIG_USB_UART_ASYNC_ADAPTER
+UART_ASYNC_ADAPTER_INST_DEFINE(async_adapter);
+#else
+static const struct device *const async_adapter;
+#endif
+
+// 初始化函数
+int uart_trans_init(void)
+{
+	if (!device_is_ready(uart_dev)) {
+		LOG_ERR("UART device not ready");
+		return -ENODEV;
+	}
+
+	int err = 0;
+	if (IS_ENABLED(CONFIG_USB_DEVICE_STACK)) {
+		int err = usb_enable(NULL);
+		if (err && (err != -EALREADY)) {
+			LOG_INF("Failed to enable USB\n");
+			return -EIO;
+		}
+	}
+
+	if (IS_ENABLED(CONFIG_USB_UART_ASYNC_ADAPTER)) {
+		/* Implement API adapter */
+		uart_async_adapter_init(async_adapter, uart_dev);
+		uart_dev = (struct device *)async_adapter;
+	}
+
+	GET_16BITS(error_frame, ERROR_HEAD_IDX) = ERROR_FRAME_HEAD;
+	GET_16BITS(error_frame, ERROR_TAIL_IDX) = ERROR_FRAME_TAIL;
+	GET_8BITS(error_frame, ERROR_ID_IDX) = HEARTBEAT_ID;
+	GET_8BITS(error_frame, ERROR_CODE_IDX) = 0x00;
+
+	err = uart_callback_set(uart_dev, uart_callback, (void *)uart_dev);
+
+	// allocate buffer and start rx
+	uart_trans_data_t *buf;
+	err = k_mem_slab_alloc(&uart_rx_slab, (void **)&buf, K_NO_WAIT);
+	__ASSERT(err == 0, "Failed to alloc slab");
+	err = uart_rx_enable(uart_dev, buf, BUF_SIZE, RX_INACTIVE_TIMEOUT);
+	__ASSERT(err == 0, "Failed to enable rx");
+
+	uart_initialized = true;
+
+	return 0;
+}
+
+// 主循环处理函数
+void uart_trans_process(void)
+{
+	int cnt = 0;
+	while (1) {
+		k_msleep(2);
+		cnt++;
+		if (uart_initialized && cnt % 50 == 0) {
+			error_handle(HEARTBEAT_ID, HEART_BEAT);
+			if (online &&
+			    (k_cyc_to_ms_near32(abs((int)last_heart_beat - (int)last_receive))) >
+				    300) {
+				online = false;
+				LOG_ERR("Connection lost for %d ms.",
+					k_cyc_to_ms_near32(k_cycle_get_32() - last_receive));
+			} else if (!online &&
+				   k_cyc_to_ms_near32(k_cycle_get_32() - last_receive) < 300) {
+				online = true;
+			}
+		}
+		process_received_data();
+	}
+}
+
+K_THREAD_DEFINE(uart_trans_thread, 2048, uart_trans_process, NULL, NULL, NULL, 1, 0, 0);
---
+++ include/ares/connectivity/uart_trans.h
@@ -0,0 +1,123 @@
+#ifndef uart_TRANS_H_
+#define uart_TRANS_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <zephyr/kernel.h>
+
+#define SYNC_FRAME_HEAD  0x5A5A
+#define SYNC_FRAME_TAIL  0x55AA
+#define SYNC_HEAD_IDX    0
+#define SYNC_ID_IDX      2
+#define SYNC_DATA_IDX    4
+#define SYNC_CRC_OFFSET  4
+#define SYNC_TAIL_OFFSET 6
+
+#define FUNC_FRAME_HEAD 0xFECA
+#define FUNC_FRAME_TAIL 0xBEBA
+#define FUNC_HEAD_IDX   0
+#define FUNC_ID_IDX     2
+#define FUNC_ARG1_IDX   4
+#define FUNC_ARG2_IDX   8
+#define FUNC_ARG3_IDX   12
+#define FUNC_REQ_IDX    16
+#define FUNC_CRC_IDX    17
+#define FUNC_TAIL_IDX   18
+
+#define ERROR_FRAME_HEAD 0xDECA
+#define ERROR_FRAME_TAIL 0xADDE
+#define ERROR_HEAD_IDX   0
+#define ERROR_ID_IDX     2
+#define ERROR_CODE_IDX   4
+#define ERROR_TAIL_IDX   6
+
+#define REPL_FRAME_HEAD  0xDEC0
+#define REPL_FRAME_TAIL  0xEFBE
+#define REPL_HEAD_IDX    0
+#define REPL_FUNC_ID_IDX 2
+#define REPL_RET_IDX     4
+#define REPL_REQ_ID_IDX  8
+#define REPL_CRC_IDX     9
+#define REPL_TAIL_IDX    10
+
+#define HEARTBEAT_ID 0xFF
+
+#define UNMATCH_CRCx sys_cpu_to_be16(BIT(0))
+#define UNKNOWN_HEAD sys_cpu_to_be16(BIT(1))
+#define UNKNOWN_TAIL sys_cpu_to_be16(BIT(2))
+#define UNKNOWN_FUNC sys_cpu_to_be16(BIT(3))
+#define UNKNOWN_SYNC sys_cpu_to_be16(BIT(4))
+#define TOOHIGH_FREQ sys_cpu_to_be16(BIT(5))
+#define CLEANED_PACK sys_cpu_to_be16(BIT(6))
+#define REQUEST_SYNC sys_cpu_to_be16(BIT(7))
+#define HEART_BEAT   sys_cpu_to_be16(BIT(8))
+
+#define SYNC_PACK_STATUS_READ  BIT(0)
+#define SYNC_PACK_STATUS_WRITE BIT(1)
+
+#define GET_8BITS(buf, n_byte)  (*(uint8_t *)(buf + n_byte))
+#define GET_16BITS(buf, n_byte) (*(uint16_t *)(buf + n_byte))
+#define GET_32BITS(buf, n_byte) (*(uint32_t *)(buf + n_byte))
+
+typedef struct id_mapping func_mapping_t;
+
+typedef uint8_t uart_trans_data_t;
+
+typedef struct sync_pack uart_sync_pack_t;
+
+typedef void (*uart_trans_cb_t)(int status);
+
+typedef void *(*uart_trans_func_t)(void *arg1, void *arg2, void *arg3);
+
+enum head_type {
+	HEAD_TYPE_SYNC = 0,
+	HEAD_TYPE_FUNC,
+	HEAD_TYPE_ERROR,
+	HEAD_TYPE_REPL,
+	HEAD_TYPE_NONE,
+};
+
+struct sync_pack {
+	uint16_t ID;
+	uart_trans_data_t *data;
+	uint8_t crc;
+	uint8_t request_id;
+	size_t len;
+	uart_trans_cb_t cb;
+	uint8_t *buf;
+};
+
+struct id_mapping {
+	uint16_t id;
+	uart_trans_func_t cb;
+};
+
+struct error_frame {
+	uint16_t head;
+	uint8_t request_id;
+	uint8_t reserved;
+	uint16_t error_code;
+	uint16_t tail;
+};
+
+// 定义一个上下文结构体来存储不完整的数据
+typedef struct {
+	uint8_t buffer[256]; // 与process_buffer大小相同
+	enum head_type head; // 当前数据的类型
+	uint16_t length;     // 当前缓冲区中的数据长度
+} PartialDataContext;
+
+int uart_trans_init();
+
+void uart_trans_func_add(uint16_t header, uart_trans_func_t cb);
+
+void uart_trans_func_remove(uint16_t header);
+
+uart_sync_pack_t *uart_trans_sync_add(uart_trans_data_t *data, uint16_t ID, size_t len,
+				      uart_trans_cb_t cb);
+
+void uart_trans_sync_flush(uart_sync_pack_t *pack);
+
+void uart_trans_call_func(uint16_t ID, void *arg1, void *arg2, void *arg3, uart_trans_cb_t cb);
+
+#endif /* uart_TRANS_H_ */
