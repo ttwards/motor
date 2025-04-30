5c1117a6f623f459a302efc4fc56ba06d08cc7e0
Wenxi Xu <xuwenxi0517@gmail.com>
2025-04-30 20:04:29 +0800

    Updates about boards

--- boards/damiao/dm_mc02/board.cmake
+++ boards/damiao/dm_mc02/board.cmake
@@ -5,7 +5,7 @@ board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd)
 
 board_runner_args(pyocd "--target=STM32H723VG")
-board_runner_args(jlink "--device=STM32H723VG" "--speed=4000")
+board_runner_args(jlink "--device=STM32H723VG" "--speed=12000")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
--- boards/dji/robomaster_board_c/board.cmake
+++ boards/dji/robomaster_board_c/board.cmake
@@ -4,7 +4,7 @@
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(openocd)
 board_runner_args(pyocd "--target=STM32F407IG")
-board_runner_args(jlink "--device=STM32F407IG" "--speed=4000")
+board_runner_args(jlink "--device=STM32F407IG" "--speed=16000")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
