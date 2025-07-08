5a40db8120ebb72ab1dc2aa860ee528b6313b16b
ttwards <12411711@mail.sustech.edu.cn>
2025-07-08 22:29:02 +0800

    修复sbus的问题（它怎么又出现了，我记得之前改好了）

--- drivers/transfer/ares_sbus.c
+++ drivers/transfer/ares_sbus.c
@@ -110,7 +110,7 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 		uint16_t len = evt->data.rx.len;
 
 		uint8_t *ptr_offset = p + find_begin(p, len);
-		if (ptr_offset >= p && ptr_offset + 25 < p + len) {
+		if (ptr_offset >= p && ptr_offset + 25 <= p + len) {
 			memcpy(data->data, ptr_offset, 25);
 		} else {
 			return;
